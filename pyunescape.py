183973273ae6c9fc6e144400ad66d5ba80498ba2
yuzhe <lazynightz@gmail.com>
2014-02-21 15:18:30 +0800

    add pyunescape

---
+++ pyunescape.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib2
+import simplejson as json
+
+
+def pyunescape(jsescapse):
+    """
+    python解析经javascrpt的escapse()加密的字符串
+    """
+    try:
+        unescaped = json.loads('"'+urllib2.unquote(jsescapse.replace('%u','\\u'))+'"')
+        return unescaped
+    except:
+        return None
+
